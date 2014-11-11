8a8588ac70fd423bc59cbf7c1d5ee6c5b91fb4bf
Raquel Bujans <look.away@gmail.com>
2014-11-11 16:07:36 -0500

    adding sorting, search, and even/odd coloring

--- app/scripts/controllers/inventory.js
+++ app/scripts/controllers/inventory.js
@@ -1,15 +1,49 @@
 'use strict';
 
 angular.module('logisticsApp.controllers')
-  .controller('InventoryCtrl', ['$scope', '$http', '$window', 'DataService', 
-  	function ($scope, $http, $window, DataService) {
-    
-    $scope.initFromData = function() {
+  .controller('InventoryCtrl', ['$scope', '$http', '$window', '$route', '$routeParams', '$location', 'DataService', 
+  	function ($scope, $http, $window, $route, $routeParams, $location, DataService) {
 
-   		$scope.other = DataService.getInventory().then(function(data) {
-    		$scope.inventory = data;
-    	});
-	};
+    $scope.index = parseInt($routeParams.index, 10);
+
+    // clear search params
+    $location.$$search = {};
+
+    $scope.sort = {
+      column: 'id',
+      descending: false
+    };
+    $scope.ascending = true;
+
+
+    $scope.initFromData = function() {
+      $scope.other = DataService.getInventory().then(function(data) {
+        $scope.inventory = data;
+      });
+    };
     $scope.$on('$viewContentLoaded', $scope.initFromData);
 
-  }]);
+    $scope.viewDetail = function(index) {
+      $location.path('inventory/' + $scope.index);
+    };
+
+    $scope.changeSorting = function(column) {
+      var sort = $scope.sort;
+      if (sort.column === column) {
+          sort.descending = !sort.descending;
+      } else {
+          sort.column = column;
+          sort.descending = false;
+      }
+      
+      $scope.ascending = !sort.descending;
+    };
+
+}]);
+
+    /*$scope.$on('$viewContentLoaded', angular.bind(this, function() {
+      DataService.getInventory().then(function(data) {
+        $scope.inventory = data;
+      });
+    }));*/
+
