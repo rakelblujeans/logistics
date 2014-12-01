d42f86fb47b1bf377beb8a511ea23bafaaebc1b1
Raquel Bujans <look.away@gmail.com>
2014-12-01 15:44:36 -0500

    select dropdown working for providers

--- app/scripts/controllers/inventory/InventoryFormCtrl.js
+++ app/scripts/controllers/inventory/InventoryFormCtrl.js
@@ -5,10 +5,13 @@ angular.module('logisticsApp.controllers')
     function ($scope, $route, $routeParams, $location, DataService) {
 
     $scope.form = { 'item': {} };
+    $scope.misc = { 'providers': [] };
     $scope.header = "New Inventory Item";
     
     $scope.initFromData = function() {
+      
       $scope.invId = parseInt($routeParams.invIndex, 10);
+      
       if ($scope.invId) { // if editing
         $scope.other = DataService.getItem($scope.invId).then(function(item) {
           $scope.form = { 'item': item };
@@ -16,29 +19,39 @@ angular.module('logisticsApp.controllers')
           var d = Date.parse($scope.form.item.last_imaged);
           $scope.form.item.last_imaged = new Date(d);
 
-          DataService.getTelcoName(item.provider_id).then(function(telcoName){
-            $scope.form.item['providerName'] = telcoName;
-          
-            /*DataService.getOrdersByPhone(item.id).then(function(orders) {
-              $scope.orders = orders;
-            });*/
+          DataService.getAll('providers').then(function(providers) {
+            $scope.misc = { 'providers': providers };
           });
+
+          if (item.provider_id) {
+            DataService.getTelcoName(item.provider_id).then(function(telcoName){
+              $scope.form.item['providerName'] = telcoName;
+            });
+          }
+
         });
       }
     };
     $scope.$on('$viewContentLoaded', $scope.initFromData);
 
     $scope.submitEdit = function(item, event) {
-      //console.log('--> Submitting form', $scope.form.item);
-      DataService.updateInventory($scope.invId, $scope.form.item).then(function(output) {
-        $location.path('inventory/' + $scope.invId);
-      });
+      console.log('--> Submitting form', $scope.form.item);
+      DataService.updateInventory($scope.invId, $scope.form.item).then(
+        function(output) {
+          $location.path('inventory/' + $scope.invId);
+        }, 
+        function() {
+          console.log('error updating inventory');
+        });
     };
 
     $scope.submitNew = function() {
       DataService.createInventory($scope.form.item).then(function(newObj) {
         $location.path('inventory/' + newObj.id);
-      });
+      }, 
+        function() {
+          console.log('error adding inventory');
+        });
     };
 
 }]);
