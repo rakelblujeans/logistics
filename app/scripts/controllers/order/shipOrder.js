d315e40b86a7489ea00af26fa471b1fc967032c3
Raquel Bujans <look.away@gmail.com>
2014-12-09 17:09:38 -0500

    detects when an order has completed shipping

--- app/scripts/controllers/order/shipOrder.js
+++ app/scripts/controllers/order/shipOrder.js
@@ -9,29 +9,37 @@ function ShipOrderCtrl($scope, $route, $routeParams, DataService, $timeout) {
       doneShipping: false
     };
 
+  function updateOrderData(orderId) {
+    DataService.getOrder(orderId).then(function(order) {
+      $scope.order = order;
+      // display a list of unshipped phones the user can choose from
+      for(var i=0; i<order.phones.length; i++) {
+      $scope.data.selection[i] = order.phones[i].id;
+      }
+
+      // build a list of which phones went out in each prior delivery.
+      // this is stricly for use in the view.
+
+      var numPhones = 0;
+      for(var j=0; j<order.shipments.length; j++) {
+        var phoneIds = [];
+        
+        for(var k=0; k<order.shipments[j].phones.length; k++) {
+          phoneIds[k] = order.shipments[j].phones[k].id;
+        }
+        $scope.data.shipmentPhoneIds[order.shipments[j].id] = '[' + phoneIds.join(',') + ']';
+        numPhones += order.shipments[j].qty;
+      }
+      if (numPhones >= order.num_phones) {
+        $scope.data.doneShipping = true;
+      }
+    });
+  }
+
   $scope.initFromData = function() {
     var orderId = parseInt($routeParams.id, 10);
     if (orderId) {
-      DataService.getOrder(orderId).then(function(order) {
-        $scope.order = order;
-        // display a list of unshipped phones the user can choose from
-        for(var i=0; i<order.phones.length; i++) {
-          $scope.data.selection[i] = order.phones[i].id;
-        }
-
-        // build a list of which phones went out in each prior delivery.
-        // this is stricly for use in the view.
-
-        for(var j=0; j<order.shipments.length; j++) {
-          var phoneIds = [];
-          
-          for(var k=0; k<order.shipments[j].phones.length; k++) {
-            phoneIds[k] = order.shipments[j].phones[k].id;
-          }
-          $scope.data.shipmentPhoneIds[order.shipments[j].id] = '[' + phoneIds.join(',') + ']';
-
-        }
-      });
+      updateOrderData(orderId);
     }
   };
   $scope.$on('$viewContentLoaded', $scope.initFromData);
@@ -63,14 +71,12 @@ function ShipOrderCtrl($scope, $route, $routeParams, DataService, $timeout) {
   };
 
   $scope.ship = function() {
-    //console.log($scope.form.shipment);
     $scope.form.shipment['phone_ids'] = $scope.data.selection;
     $scope.form.shipment['order_id'] = $scope.order.id;
     $scope.form.shipment['out_on_date'] = new Date().toISOString();
 
     DataService.createShipment($scope.form).then(function(new_shipment) {
-      //console.log("received: ", new_shipment);
-      $scope.data.pastShipments[$scope.data.pastShipments.length] = new_shipment;
+      updateOrderData($scope.orderId);
     });
   };
 
